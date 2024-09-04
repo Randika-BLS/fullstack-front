6c896ef35a3e4929d2cc998877ce3528056ba31e
Your Name <you@example.com>
2024-09-04 12:03:12 +0530

    Added

--- src/users/ViewUser.js
+++ src/users/ViewUser.js
@@ -1,7 +1,25 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
 
 export default function ViewUser() {
+  const [user, setUser] = useState({
+    name: "",
+    username: "",
+    email: "",
+  });
+
+  const { id } = useParams();
+
+  useEffect(() => {
+    loadUsers();
+  }, []);
+
+  const loadUsers = async () => {
+    const result = await axios.get(`http://localhost:8080/user/${id}`);
+    setUser(result.data);
+  };
+
   return (
     <div className="container">
       <div className="row">
@@ -10,16 +28,19 @@ export default function ViewUser() {
 
           <div className="card">
             <div className="card-header">
-              Details of user id :
+              Details of user id : {user.id}
               <ul className="list-group list-group-flush">
                 <li className="list-group-item">
                   <b>Name:</b>
+                  {user.name}
                 </li>
                 <li className="list-group-item">
                   <b>UserName:</b>
+                  {user.username}
                 </li>
                 <li className="list-group-item">
                   <b>Email:</b>
+                  {user.email}
                 </li>
               </ul>
             </div>
